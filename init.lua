5fd97d1b23e8fa59ba95458c76f7b2097a0b37cd
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-25 21:55:25 +0200

    Add clay, diamond ore, diamond block, ruby block, birch log, birch planks, spruce log, spruce planks, glowstone, nether reactor, chair and table

--- init.lua
+++ init.lua
@@ -65,10 +65,129 @@ register('cobweb', {
 	liquid_range = 0,
 	walkable = false,
 })
+register('clay', {
+	order = 71,
+	description = 'Clay',
+	tiles = { terrain(85) },
+	sound = 'dirt',
+})
 
 register('cyan_flower', {
-	order = 71,
+	order = 72,
 	description = 'Cyan Flower',
 	inventory_image = terrain2(2),
 	sound = 'grass',
+})
+
+register('diamond_ore', {
+	order = 73,
+	description = "Diamond Ore",
+	tiles = { terrain2(3) },
+	sound = 'stone',
+})
+register('diamond_block', {
+	order = 74,
+	description = "Block of Diamond",
+	tiles = {
+		terrain2(16),
+		terrain2(48),
+		terrain2(32)},
+	sound = 'metal',
+})
+
+register('ruby_block', {
+	order = 75,
+	description = "Ruby Block",
+	tiles = {
+		terrain2(17),
+		terrain2(49),
+		terrain2(33)},
+	sound = 'metal',
+})
+
+register('birch_log', {
+	order = 76,
+	description = "Birch Log",
+	tiles = {
+		terrain2(19),
+		terrain2(19),
+		terrain2(18)},
+	sound = 'wood',
+})
+register('birch_planks', {
+	order = 77,
+	description = "Birch Planks",
+	tiles = { terrain2(20) },
+	sound = 'wood',
+})
+
+register('spruce_log', {
+	order = 78,
+	description = "Spruce Log",
+	tiles = {
+		terrain2(35),
+		terrain2(35),
+		terrain2(34)},
+	sound = 'wood',
+})
+register('spruce_planks', {
+	order = 79,
+	description = "Spruce Planks",
+	tiles = { terrain2(36) },
+	sound = 'wood',
+})
+
+register('glowstone', {
+	order = 80,
+	description = "Glowstone",
+	tiles = { terrain2(4) },
+	light_source = minetest.LIGHT_MAX-1,
+	sound = 'glass',
+})
+
+register('nether_reactor', {
+	order = 81,
+	description = "Nether Reactor",
+	tiles = { terrain2(5) },
+	sound = 'glass',
+})
+
+register('chair', {
+	order = 82,
+	description = "Chair",
+	tiles = { terrain(4) },
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.125, -0.25, 0.25, 0, 0.25},
+			{-0.25, -0.5, -0.25, -0.125, -0.125, -0.125},
+			{-0.25, -0.5, 0.125, -0.125, -0.125, 0.25},
+			{0.125, -0.5, 0.125, 0.25, -0.125, 0.25},
+			{0.125, -0.5, -0.25, 0.25, -0.125, -0.125},
+			{-0.25, 0, 0.125, 0.25, 0.5, 0.25},
+		}
+	},
+	sound = 'wood',
+})
+
+register('table', {
+	order = 83,
+	description = "Table",
+	tiles = { terrain(4) },
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.125, -0.25, 0.25, 0, 0.25},
+			{-0.25, -0.5, -0.25, -0.125, -0.125, -0.125},
+			{-0.25, -0.5, 0.125, -0.125, -0.125, 0.25},
+			{0.125, -0.5, 0.125, 0.25, -0.125, 0.25},
+			{0.125, -0.5, -0.25, 0.25, -0.125, -0.125}
+		}
+	},
+	sound = 'wood',
 })
